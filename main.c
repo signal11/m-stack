71a8b29794a48e032694d18fb100db3adb0b8863
Alan Ott <alan@signal11.us>
2013-04-09 16:15:39 -0400

    main: Some basic test code

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ Signal 11 Software
 
 #include "usb.h"
 #include <xc.h>
+#include <string.h>
+#include "usb_config.h"
 
 _CONFIG1(WDTPS_PS16 & FWPSA_PR32 & WINDIS_OFF & FWDTEN_OFF & ICS_PGx1 & GWRP_OFF & GCP_OFF & JTAGEN_OFF)
 _CONFIG2(POSCMOD_NONE & I2C1SEL_PRI & IOL1WAY_OFF & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_NODIV & IESO_OFF)
@@ -18,12 +20,18 @@ _CONFIG4(DSWDTPS_DSWDTPS3 & DSWDTOSC_SOSC & RTCOSC_SOSC & DSBOREN_OFF & DSWDTEN_
 int main(void)
 {
 	unsigned int pll_startup_counter = 600;
-        CLKDIVbits.PLLEN = 1;
-        while(pll_startup_counter--);
+	CLKDIVbits.PLLEN = 1;
+	while(pll_startup_counter--);
 
 	usb_init();
+	
+	uchar *buf = usb_get_in_buffer(1);
+	memset(buf, 0xa0, EP_1_IN_LEN);
 
 	while (1) {
+	if (!usb_in_endpoint_busy(1))
+		usb_send_in_buffer(1, 64);
+
 		usb_service();
 	}
 
