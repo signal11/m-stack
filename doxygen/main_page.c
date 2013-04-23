d16948ca97c51d96437016b05766432e0a86c74f
Alan Ott <alan@signal11.us>
2013-04-22 21:27:34 -0400

    doxygen: Add main page
    This isn't a real C file, but a C file with doxygen comments which set up
    the main page. There's not much in it right now.

---
+++ doxygen/main_page.c
@@ -0,0 +1,9 @@
+/**
+ @mainpage Signal 11 USB Stack
+ 
+ @section Intro
+ This is the Signal 11 USB Stack. For more information, see the <a href="group__public__api.html">Public API Page.</a>
+ 
+ */
+ 
+ 
