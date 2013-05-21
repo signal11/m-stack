32adade0be324e7280a606adaaa17279ab2b7c7d
Alan Ott <alan@signal11.us>
2013-05-20 20:00:59 -0400

    doxygen: Add web page link to the main doxygen page

--- doxygen/main_page.c
+++ doxygen/main_page.c
@@ -2,7 +2,11 @@
  @mainpage M-Stack
  
  @section Intro
- This is M-Stack, a free USB Device Stack for PIC Microcontrollers. For more information, see the <a href="group__public__api.html">Public API Page.</a>
+ This is M-Stack, a free USB Device Stack for PIC Microcontrollers.
+
+ For more information, see the <a href="http://www.signal11.us/oss/m-stack">main web page.</a>
+
+ For API documentation, see the <a href="group__public__api.html">Public API Page.</a>
  
  */
  
