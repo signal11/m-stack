20c18197fd4b881379165c26a6b794443c8f21c0
Alan Ott <alan@signal11.us>
2013-09-21 09:28:08 -0400

    apps, host_test: Add warranty disclaimer to header
    In the example files which are not under the dual LGPL/Apache license (ie:
    those files which are under a "use for any purpose" license), add a
    warranty disclaimer section.

--- apps/hid_mouse/usb_config.h
+++ apps/hid_mouse/usb_config.h
@@ -7,6 +7,13 @@
  * It is worth noting that M-Stack itself is not under the same license
  * as this file.
  *
+ * M-Stack is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  For details, see sections 7, 8, and 9
+ * of the Apache License, version 2.0 which apply to this file.  If you have
+ * purchased a commercial license for this software from Signal 11 Software,
+ * your commerical license superceeds the information in this header.
+ *
  * Alan Ott
  * Signal 11 Software
  * 2013-04-08
