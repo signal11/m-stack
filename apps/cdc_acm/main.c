6323cbc4e1968150a3c8cc9cbc86cfb718ac01e9
Alan Ott <alan@signal11.us>
2016-05-03 17:33:02 -0400

    apps/cdc_acm: Use common/hardware.c for hardware initialization

--- apps/cdc_acm/main.c
+++ apps/cdc_acm/main.c
@@ -25,88 +25,7 @@
 #include "usb_config.h"
 #include "usb_ch9.h"
 #include "usb_cdc.h"
-
-#ifdef __PIC32MX__
-	#include <plib.h>
-#endif
-
-#ifdef __PIC24FJ64GB002__
-_CONFIG1(WDTPS_PS16 & FWPSA_PR32 & WINDIS_OFF & FWDTEN_OFF & ICS_PGx1 & GWRP_OFF & GCP_OFF & JTAGEN_OFF)
-_CONFIG2(POSCMOD_NONE & I2C1SEL_PRI & IOL1WAY_OFF & OSCIOFNC_OFF & FCKSM_CSDCMD & FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_NODIV & IESO_OFF)
-_CONFIG3(WPFP_WPFP0 & SOSCSEL_IO & WUTSEL_LEG & WPDIS_WPDIS & WPCFG_WPCFGDIS & WPEND_WPENDMEM)
-_CONFIG4(DSWDTPS_DSWDTPS3 & DSWDTOSC_SOSC & RTCOSC_SOSC & DSBOREN_OFF & DSWDTEN_OFF)
-
-#elif __PIC24FJ256DA206__
-_CONFIG1(WDTPS_PS32768 & FWPSA_PR128 & WINDIS_OFF & FWDTEN_OFF & ICS_PGx2 & GWRP_OFF & GCP_OFF & JTAGEN_OFF)
-_CONFIG2(POSCMOD_NONE & IOL1WAY_OFF & OSCIOFNC_ON & FCKSM_CSECMD & FNOSC_FRCPLL & PLL96MHZ_ON & PLLDIV_NODIV & IESO_OFF)
-_CONFIG3(WPFP_WPFP255 & SOSCSEL_SOSC & WUTSEL_LEG & ALTPMP_ALPMPDIS & WPDIS_WPDIS & WPCFG_WPCFGDIS & WPEND_WPENDMEM)
-
-#elif _18F46J50
-#pragma config PLLDIV = 3 /* 3 = Divide by 3. 12MHz crystal => 4MHz */
-#pragma config XINST = OFF
-#pragma config WDTEN = OFF
-#pragma config CPUDIV = OSC1
-#pragma config IESO = OFF
-#pragma config FCMEN = OFF
-#pragma config LPT1OSC = OFF
-#pragma config T1DIG = ON
-#pragma config OSC = ECPLL
-#pragma config DSWDTEN = OFF
-#pragma config IOL1WAY = OFF
-#pragma config WPDIS = OFF /* This pragma seems backwards */
-
-#elif _16F1459
-#pragma config FOSC = INTOSC
-#pragma config WDTE = OFF
-#pragma config PWRTE = OFF
-#pragma config MCLRE = ON
-#pragma config CP = OFF
-#pragma config BOREN = ON
-#pragma config CLKOUTEN = OFF
-#pragma config IESO = OFF
-#pragma config FCMEN = OFF
-#pragma config WRT = OFF
-#pragma config CPUDIV = NOCLKDIV
-#pragma config USBLSCLK = 48MHz
-#pragma config PLLMULT = 3x
-#pragma config PLLEN = ENABLED
-#pragma config STVREN = ON
-#pragma config BORV = LO
-#pragma config LPBOR = ON
-#pragma config LVP = OFF
-
-#elif _16F1454
-#pragma config FOSC = INTOSC
-#pragma config WDTE = OFF
-#pragma config PWRTE = OFF
-#pragma config MCLRE = ON
-#pragma config CP = OFF
-#pragma config BOREN = ON
-#pragma config CLKOUTEN = OFF
-#pragma config IESO = OFF
-#pragma config FCMEN = OFF
-#pragma config WRT = OFF
-#pragma config CPUDIV = NOCLKDIV
-#pragma config USBLSCLK = 48MHz
-#pragma config PLLMULT = 3x
-#pragma config PLLEN = ENABLED
-#pragma config STVREN = ON
-#pragma config BORV = LO
-#pragma config LPBOR = ON
-#pragma config LVP = OFF
-
-#elif __32MX460F512L__
-#pragma config DEBUG = OFF, ICESEL = ICS_PGx2, PWP = OFF, BWP = OFF, CP = OFF
-#pragma config FNOSC = PRIPLL, FSOSCEN = OFF, IESO = OFF, POSCMOD = HS, \
-	       OSCIOFNC = OFF, FPBDIV = DIV_1, FCKSM = CSDCMD, WDTPS = PS1, \
-	       FWDTEN = OFF
-#pragma config FPLLIDIV = DIV_2, FPLLMUL = MUL_15, UPLLIDIV = DIV_2, \
-	       UPLLEN = ON, FPLLODIV = DIV_1
-
-#else
-	#error "Config flags for your device not defined"
-
-#endif
+#include "hardware.h"
 
 #ifdef MULTI_CLASS_DEVICE
 static uint8_t cdc_interfaces[] = { 0 };
@@ -127,37 +46,8 @@ static void send_string_sync(uint8_t endpoint, const char *str)
 
 int main(void)
 {
-#if defined(__PIC24FJ64GB002__) || defined(__PIC24FJ256DA206__)
-	unsigned int pll_startup_counter = 600;
-	CLKDIVbits.PLLEN = 1;
-	while(pll_startup_counter--);
-#elif _18F46J50
-	unsigned int pll_startup = 600;
-	OSCTUNEbits.PLLEN = 1;
-	while (pll_startup--)
-		;
-#elif _16F1459 || _16F1454
-	OSCCONbits.IRCF = 0b1111; /* 0b1111 = 16MHz HFINTOSC postscalar */
 
-	/* Enable Active clock-tuning from the USB */
-	ACTCONbits.ACTSRC = 1; /* 1=USB */
-	ACTCONbits.ACTEN = 1;
-#elif __32MX460F512L__
-	SYSTEMConfigPerformance(80000000);
-#endif
-
-
-/* Configure interrupts, per architecture */
-#ifdef USB_USE_INTERRUPTS
-	#if defined (_PIC18) || defined(_PIC14E)
-		INTCONbits.PEIE = 1;
-		INTCONbits.GIE = 1;
-	#elif __PIC32MX__
-		INTCONbits.MVEC = 1; /* Multi-vector interrupts */
-		IPC11bits.USBIP = 4; /* Interrupt priority, must set to != 0. */
-		__asm volatile("ei");
-	#endif
-#endif
+    hardware_init();
 
 #ifdef MULTI_CLASS_DEVICE
 	cdc_set_interface_list(cdc_interfaces, sizeof(cdc_interfaces));
